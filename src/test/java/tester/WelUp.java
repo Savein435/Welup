d35d3d9eb8eb7477bd7b06d28c4ef1a3b6c993ba
ayush737 <ayush@apples-MacBook-Pro-6.local>
2024-12-11 15:26:47 +0530

    Chatggpt Changes

--- src/test/java/tester/WelUp.java
+++ src/test/java/tester/WelUp.java
@@ -1,241 +1,207 @@
 package tester;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.Test;
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import com.google.common.base.Verify;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-import java.lang.Thread;
 import java.time.Duration;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WelUp {
 
-   // public WebDriver driver;
-    WebDriver driver = new ChromeDriver();
+    WebDriver driver;
     String URL = "https://qa.welup.savein.money/signup";
     
-    
-    
-    
-
     @BeforeMethod
-    public void setup() throws InterruptedException {
-        // Use WebDriverManager to handle driver binaries
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-        //driver.get("https://www.savein.money");
-        //driver.get("https://www.savein.money");	
-        
-        
-        
+    public void setup() {
+        // Set up WebDriver and configure the browser settings
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
     }
-    @Test (priority = 1)
-    public void EmployerOnboard() throws InterruptedException
-    {
-    	driver.get(URL);	
-    	driver.manage().window().maximize();
-        Thread.sleep(20);
-        //Name
-        WebElement Name = driver.findElement(By.cssSelector("body > div:nth-child(1) > main:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(4) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > input:nth-child(1)"));
-        Name.click();
-        Name.clear();
-        Name.sendKeys("Ayush Tiwari");
-        //
-        Thread.sleep(200);
-        //Mobile number 
-        WebElement Mobile = driver.findElement(By.cssSelector("body > div:nth-child(1) > main:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(4) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > input:nth-child(2)"));
-        Mobile.click();
-        Mobile.sendKeys("398778");
-        System.out.println("Invalid Number Check Passed");
-        Mobile.sendKeys("2873849723");
-        System.out.println("Valid Number Check Passed");
-        Thread.sleep(200);
-        //Email
-        WebElement Email = driver.findElement(By.cssSelector("body > div:nth-child(1) > main:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(4) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > input:nth-child(1)"));
-        Email.clear();
-        Email.sendKeys("jkhdjk32223@gmail.com");
-        System.out.println("Valid Email Check Passed");
-        Thread.sleep(2000);
-        //Send OTP 
-        WebElement Send = driver.findElement(By.xpath("//button[normalize-space()='Send OTP']"));
-        Send.click();
-        Thread.sleep(200);
-        //Enter OTP
-        WebElement OTP = driver.findElement(By.xpath("//div[@id='otp']//div[1]//input[1]"));
-        OTP.sendKeys("1111111");
-        System.out.println("Valid OTP Check Passed");
-        Thread.sleep(200);
-        //Verify OTP
-        WebElement verify = driver.findElement(By.xpath("//button[normalize-space()='Verify']"));
-        verify.click();
-        Thread.sleep(200);
-        //Select Designation
-        WebElement Designation = driver.findElement(By.xpath("//button[normalize-space()='HR']"));
-        Designation.click();
-        Thread.sleep(200);
-        // Submit Detail
-        WebElement Submit = driver.findElement(By.xpath("//button[normalize-space()='Continue']"));
-        Submit.click();
-        Thread.sleep(200);
-        
-        
-        // Company Name Entry
-        
-        //Couldn't Find your company
-        WebElement Manual = driver.findElement(By.xpath("//button[normalize-space()='Click here']"));
-        Manual.click();
-        Thread.sleep(200);
-        //Type Your Company name 
-        WebElement CompanyEntry = driver.findElement(By.xpath("//input[@id='inputid']"));
-        CompanyEntry.clear();
-        CompanyEntry.sendKeys("Test Company Entry");
-        System.out.println("Manual Entry Of Company Name");
-        Thread.sleep(200);
-        
-        
-        //Continue Button
-        WebElement Continue2 = driver.findElement(By.cssSelector("//button[normalize-space()='Continue']"));
-        Continue2.click();
-        System.out.println("Continue Button Clicked");
-        Thread.sleep(200);
-         
-        	
-        // Plan Overview Continue
-        Continue2.click();
-        System.out.println("Plan Selected");
-        Thread.sleep(200);
-        
-        //Book Activation Meeting Popup
-        //Book Activation meeting 
-        
-        WebElement Book = driver.findElement(By.cssSelector(".ml-2"));
-        Book.click();
-        
-        
-        
-        
-       
-        
+    
+    @AfterMethod
+    public void cleanup() {
+        // Close the browser after each test
+        if (driver != null) {
+            driver.quit();
         }
-    @Test (priority = 2)
-    public void Login() throws InterruptedException
-    {
-    	driver.get("https://qa.welup.savein.money/login");	
-    	driver.manage().window().maximize();
-        Thread.sleep(20);
+    }
+
+    @Test(priority = 1)
+    public void EmployerOnboard() throws InterruptedException {
+        driver.get(URL);   
+        
+        // Enter Name
+        enterName("Ayush Tiwari");
+
+        // Enter Mobile Number
+        enterMobileNumber("3987782873849723");
+
         // Enter Email
-        WebElement Email = driver.findElement(By.xpath("//input[@id='inputid']"));
-       Email.click();
-       Email.clear();
-       Email.sendKeys("anurag.singh@savein.money");
-       System.out.println("Email Entered ");
-       Thread.sleep(200);
-       
-       //Continue Click 
-       WebElement Continue = driver.findElement(By.xpath("//button[normalize-space()='Continue']"));
-       Continue.click();
-       
-       //Submit OTP
-       WebElement EnterOTP = driver.findElement(By.xpath("//div[@id='otp']//div[1]//input[1]"));
-       EnterOTP.click();
-       EnterOTP.sendKeys("111111");
-       
-       //submit Otp ;
-       WebElement VerifyOTP = driver.findElement(By.xpath("//button[normalize-space()='Login']"));
-       VerifyOTP.click();
-       
-       
-       
-    	
-    }
-    @Test (priority = 3)
-    public void AddEmployee() throws InterruptedException
-    {
-    	//Employee Login
-    	Login();
-    	
-    	//Select Employee Management
-    	WebElement EmployeeManagement = driver.findElement(By.xpath("//button[normalize-space()='Employee Management']"));
-    	EmployeeManagement.click();
-    	
-    	System.out.print("PAssed");
-    	
-    	//Add Employee Button Click
-    	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); 
-        WebElement AddEmployee = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//button[contains(text(),'Add Employee')])[1]")));
-    	AddEmployee.click();
-    	
-    	//Add Employees Via Form 
-    	WebElement Form = driver.findElement(By.cssSelector("span[class='border-b border-black']"));
-    	Form.click();
-    	
-    	   // Form Entry 
-    	   //Name
-    	WebElement EmployeeName = driver.findElement(By.cssSelector("input[placeholder='Enter Employee Name']"));
-    	EmployeeName.clear();
-    	EmployeeName.sendKeys("Ayush Tiwari");
-    	
-    	    //Email
-    	WebElement WorkEmail = driver.findElement(By.xpath("//input[@placeholder='Enter Your  Email ID']"));
-    	WorkEmail.clear();
-    	WorkEmail.sendKeys("randomworkemail@gmail.com");
-    	
-    	    //Enter Phone Number
-    	WebElement  WorkPhone = driver.findElement(By.cssSelector("input[placeholder='Enter phone number']"));
-    	WorkPhone.clear();
-    	WorkPhone.sendKeys("9928492949");
-    	
-    	   // Employee Code
-    	WebElement ECODE = driver.findElement(By.cssSelector("input[placeholder='Enter employee code']"));
-    	ECODE.sendKeys("SI38390");
-    	
-    	
-    	//Submit Detail
-    	WebElement SubmitDetail = driver.findElement(By.cssSelector("button[type='submit']"));
-    	SubmitDetail.click();
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
+        enterEmail("jkhdjk32223@gmail.com");
+
+        // Send OTP and Verify
+        sendOTP("1111111");
+
+        // Select Designation
+        selectDesignation("HR");
+
+        // Submit Details
+        submitDetails();
+
+        // Manual Company Name Entry
+        enterCompanyName("Test Company Entry");
+
+        // Continue Button Click
+        clickContinueButton();
+
+        // Plan Overview Continue
+        clickContinueButton();
+        
+        // Book Activation Meeting
+        bookActivationMeeting();
     }
 
-    @Test (priority = 4)
-    public void DeleteEmployee() throws InterruptedException
-    {
-    	//Employee Login
-    	Login();
-    	
-    	//Select Employee Management
-    	WebElement EmployeeManagement = driver.findElement(By.xpath("//button[normalize-space()='Employee Management']"));
-    	EmployeeManagement.click();
-    	
-    	System.out.println("PAssed");
-    	
+    @Test(priority = 2)
+    public void Login() throws InterruptedException {
+        driver.get("https://qa.welup.savein.money/login");
+
+        // Enter Email
+        enterEmail("anurag.singh@savein.money");
+
+        // Continue and submit OTP
+        submitOTP("111111");
     }
-    	
-    
-    
-    
-   
-   
-	
+
+    @Test(priority = 3)
+    public void AddEmployee() throws InterruptedException {
+        // Employee Login
+        Login();
+
+        // Navigate to Employee Management
+        WebElement employeeManagement = driver.findElement(By.xpath("//button[normalize-space()='Employee Management']"));
+        employeeManagement.click();
+
+        // Add Employee Button Click
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); 
+        WebElement addEmployee = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//button[contains(text(),'Add Employee')])[1]")));
+        addEmployee.click();
+
+        // Add Employees via Form
+        addEmployeeViaForm("Ayush Tiwari", "randomworkemail@gmail.com", "9928492949", "SI38390");
+    }
+
+    @Test(priority = 4)
+    public void DeleteEmployee() throws InterruptedException {
+        // Employee Login
+        Login();
+
+        // Navigate to Employee Management
+        WebElement employeeManagement = driver.findElement(By.xpath("//button[normalize-space()='Employee Management']"));
+        employeeManagement.click();
+
+        // Add logic for deleting employee if necessary
+    }
+
+    // Helper Methods
+    private void enterName(String name) {
+        WebElement nameField = driver.findElement(By.cssSelector("input[placeholder='Enter your name']"));
+        nameField.clear();
+        nameField.sendKeys(name);
+    }
+
+    private void enterMobileNumber(String mobile) {
+        WebElement mobileField = driver.findElement(By.cssSelector("input[placeholder='Enter mobile number']"));
+        mobileField.clear();
+        mobileField.sendKeys(mobile);
+    }
+
+    private void enterEmail(String email) {
+        WebElement emailField = driver.findElement(By.cssSelector("input[placeholder='Enter email']"));
+        emailField.clear();
+        emailField.sendKeys(email);
+    }
+
+    private void sendOTP(String otp) throws InterruptedException {
+        WebElement sendButton = driver.findElement(By.xpath("//button[normalize-space()='Send OTP']"));
+        sendButton.click();
+        Thread.sleep(1000);
+
+        WebElement otpField = driver.findElement(By.xpath("//div[@id='otp']//div[1]//input[1]"));
+        otpField.sendKeys(otp);
+
+        WebElement verifyButton = driver.findElement(By.xpath("//button[normalize-space()='Verify']"));
+        verifyButton.click();
+    }
+
+    private void selectDesignation(String designation) {
+        WebElement designationButton = driver.findElement(By.xpath("//button[normalize-space()='" + designation + "']"));
+        designationButton.click();
+    }
+
+    private void submitDetails() {
+        WebElement submitButton = driver.findElement(By.xpath("//button[normalize-space()='Continue']"));
+        submitButton.click();
+    }
+
+    private void enterCompanyName(String companyName) {
+        WebElement manualEntryButton = driver.findElement(By.xpath("//button[normalize-space()='Click here']"));
+        manualEntryButton.click();
+
+        WebElement companyEntryField = driver.findElement(By.xpath("//input[@id='inputid']"));
+        companyEntryField.clear();
+        companyEntryField.sendKeys(companyName);
+    }
+
+    private void clickContinueButton() {
+        WebElement continueButton = driver.findElement(By.xpath("//button[normalize-space()='Continue']"));
+        continueButton.click();
+    }
+
+    private void bookActivationMeeting() {
+        WebElement bookButton = driver.findElement(By.cssSelector(".ml-2"));
+        bookButton.click();
+    }
+
+    private void submitOTP(String otp) {
+        WebElement otpField = driver.findElement(By.xpath("//div[@id='otp']//div[1]//input[1]"));
+        otpField.click();
+        otpField.sendKeys(otp);
+
+        WebElement submitButton = driver.findElement(By.xpath("//button[normalize-space()='Login']"));
+        submitButton.click();
+    }
+
+    private void addEmployeeViaForm(String name, String email, String phone, String employeeCode) {
+        WebElement formButton = driver.findElement(By.cssSelector("span[class='border-b border-black']"));
+        formButton.click();
+
+        WebElement nameField = driver.findElement(By.cssSelector("input[placeholder='Enter Employee Name']"));
+        nameField.clear();
+        nameField.sendKeys(name);
+
+        WebElement emailField = driver.findElement(By.xpath("//input[@placeholder='Enter Your Email ID']"));
+        emailField.clear();
+        emailField.sendKeys(email);
+
+        WebElement phoneField = driver.findElement(By.cssSelector("input[placeholder='Enter phone number']"));
+        phoneField.clear();
+        phoneField.sendKeys(phone);
+
+        WebElement employeeCodeField = driver.findElement(By.cssSelector("input[placeholder='Enter employee code']"));
+        employeeCodeField.sendKeys(employeeCode);
+
+        WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        submitButton.click();
+    }
 }
-
